a6adb0ca8726c43c991a7425657cbc5952e3f14b
zhardy <zhardy177@gmail.com>
2015-07-05 12:36:42 -0400

    full title edit functionality

--- netflix-remote-website/test/js/test.js
+++ netflix-remote-website/test/js/test.js
@@ -1,36 +1,31 @@
+var cancel_button = "<span class='glyphicon glyphicon-remove cancel-button'></span>";
+var okay_button   = "<span class='glyphicon glyphicon-ok okay-button'></span>";
+var edit_button   = "<span class='glyphicon glyphicon-pencil edit-button'></span>";
+
 
    
 $( document ).ready(function() {
+    menu_toggle();
+    add_button();
     playlist_click();
-	    
-    $("#menu-toggle").click(function(event) {
-    	event.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-        
-    });
+    edit();    
+});
 
+function menu_toggle(){
+    $("#menu-toggle").click(function(event) {
+        event.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+}
+
+function add_button(){
     $("#add").click(function(event){
-    	event.preventDefault();
-    	$(this).before("<li class='playlist'><a href='#''>Test</a></li>");
-    	$(this).removeClass('selected');
+        event.preventDefault();
+        $(this).before("<li class='playlist'><a href='#''>Test</a></li>");
+        $(this).removeClass('selected');
         playlist_click();
     });
-
-    $(".edit-button").click(function(event){
-        var item = $(this).parent();
-        var title = $(this).parent().html();
-        title = title.split(" <sp"); 
-        title = title[0].trim();
-        item.empty();
-        item.append("<input class='playlist-title-input' type='text' value='"+ title +"'>");
-        text_input_select();
-        $('.playlist-title-input').focus();
-        // ?
-
-        
-    });
-    
-});
+}
 
 
 function playlist_click(){
@@ -40,8 +35,38 @@ function playlist_click(){
     });
 }
 
-function text_input_select(){
+function edit(){
+    $(".edit-button").click(function(event){
+        var item = $(this).parent();
+        var title = $(this).parent().html();
+        title = title.split("<sp"); 
+        title = title[0].trim();
+        item.empty();
+        item.append("<input class='playlist-title-input' type='text' value='"+ title +"'>");
+        item.append( okay_button + cancel_button);
+        buttons_and_input(title);
+        $('.playlist-title-input').focus();        
+    });
+}
+
+function buttons_and_input(title){
+
     $('.playlist-title-input').focus(function() {
         $(this).select();
+    });
+
+    $('.cancel-button').click(function(event){
+        var item = $(this).parent();
+        item.empty();
+        item.append(title + edit_button);
+        edit();
+    });
+
+    $('.okay-button').click(function(event){
+        var value = $('.playlist-title-input').val();
+        var item = $(this).parent();
+        item.empty();
+        item.append(value + edit_button);
+        edit();
     });
 }
